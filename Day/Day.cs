cb51363908ea3116b2852e97b313e720bb3169c8
anchik <khaikinaanna@gmail.com>
2015-08-16 21:12:33 +0300

    commit solution and config files

---
+++ Day/Day.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDay
+{
+    public class Day
+    {
+        public const string CorrectWakeUpTimeMessage = "Good morning!";
+        public const string LateWakeUpTimeMessage = "Too late!";
+
+        DateTime? requiredWakeUpTime;
+        DateTime actualWakeUpTime;
+
+        public DateTime Date { get; set; }
+        public DateTime? RequiredWakeUpTime { get { return requiredWakeUpTime; } }
+        public DateTime ActualWakeUpTime { get { return actualWakeUpTime; } }
+
+        public Day()
+        {
+            Date = DateTime.Now.Date;
+        }
+
+        public Day(DateTime date)
+        {
+            Date = date.Date;
+        }
+
+        public void SetRequiredWakeUpTime(int hour, int minute)
+        {
+            requiredWakeUpTime = new DateTime(Date.Year, Date.Month, Date.Day, hour, minute, 0);
+        }
+
+        public string WakeUp(int hour, int minute)
+        {
+            actualWakeUpTime = new DateTime(Date.Year, Date.Month, Date.Day, hour, minute, 0);
+
+            if (actualWakeUpTime <= requiredWakeUpTime||requiredWakeUpTime==null) return CorrectWakeUpTimeMessage;
+            else return LateWakeUpTimeMessage;
+        }
+
+
+    }
+}
---
+++ Tests/DayTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyDay;
+
+namespace Tests
+{
+    [TestClass]
+    public class DayTest
+    {
+        [TestMethod]
+        public void WakeUp_WhenActualTimeIsLaterThanRequired_ShouldReturnLateWakeUpTimeMessage()
+        {
+            // arrange
+            Day day = new Day();
+            day.SetRequiredWakeUpTime(7, 0);
+
+
+            // act
+            string result = day.WakeUp(8, 0);
+            
+            // assert
+            StringAssert.Contains(result, Day.LateWakeUpTimeMessage);
+                
+        }
+
+        [TestMethod]
+        public void WakeUp_WhenActualTimeIsearlierThanRequired_ShouldReturnCorrectWakeUpTimeMessage()
+        {
+            // arrange
+            Day day = new Day();
+            day.SetRequiredWakeUpTime(7, 0);
+
+
+            // act
+            string result = day.WakeUp(6, 0);
+
+            // assert
+            StringAssert.Contains(result, Day.CorrectWakeUpTimeMessage);
+
+        }
+
+        
+    }
+}
